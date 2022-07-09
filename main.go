3e125df9992618c80ecc2e6d9aba4fe6845983b2
Darren <dleak22@gmail.com>
2022-07-09 09:18:40 +0200

    test

--- main.go
+++ main.go
@@ -2,12 +2,7 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"os"
-	"path/filepath"
-	"sync"
-
-	"gopkg.in/yaml.v3"
+	"os/exec"
 )
 
 type Runnable struct {
@@ -46,123 +41,148 @@ var allRunnables = make(map[string]map[string]Runnable)
 var channels = make(map[string]chan string)
 
 func main() {
-	actionToRun := os.Args[1]
-	allRunnables = setupCommands([]string{"test.yml"})
+	// actionToRun := os.Args[1]
+	// allRunnables = setupCommands([]string{"/Users/darren/Developer/Projects/CLIManager/test.yml"})
 
 	// TODO: This action will come in from the cli
-	runAction(actionToRun)
-}
+	// runAction(actionToRun)
 
-var dependentsMap = make(map[string][]string)
-
-// TODO: Needs to be split up
-func setupCommands(filePaths []string) map[string]map[string]Runnable {
-	var runnablesStatuses = make(map[string]map[string]Runnable)
-
-	for _, filePath := range filePaths {
-		filename, _ := filepath.Abs(filePath)
-		yamlFile, err := ioutil.ReadFile(filename)
-
-		if err != nil {
-			panic(err)
-		}
-
-		actions := Actions{}
-		err = yaml.Unmarshal(yamlFile, &actions)
-
-		if err != nil {
-			panic("Unmarshal failed")
-		}
-
-		for _, currentAction := range actions.Actions {
-			runnables := currentAction.Runnables
-			runnablesStatuses[currentAction.Name] = make(map[string]Runnable)
-
-			for _, runnable := range runnables {
-				runnablesStatuses[currentAction.Name][runnable.Name] = runnable
-			}
-
-			for _, runnable := range runnables {
-				for _, dependent := range runnable.DependsOn {
-					dependentsMap[dependent] = append(dependentsMap[dependent], runnable.Name)
-				}
-			}
-		}
+	runnable := Runnable{
+		Command:   "nvm use default",
+		Name:      "",
+		Alias:     "",
+		DependsOn: []string{""},
 	}
 
-	return runnablesStatuses
+	channel := make(chan string)
+	runCommand(runnable, channel)
+
+	fmt.Println("DONE")
 }
 
-func runAction(actionName string) {
-	runnables := allRunnables[actionName]
-	feedbackChannel := make(chan string)
+// var dependentsMap = make(map[string][]string)
 
-	defer close(feedbackChannel)
+// // TODO: Needs to be split up
+// func setupCommands(filePaths []string) map[string]map[string]Runnable {
+// 	var runnablesStatuses = make(map[string]map[string]Runnable)
 
-	var wg sync.WaitGroup
+// 	for _, filePath := range filePaths {
+// 		filename, _ := filepath.Abs(filePath)
+// 		yamlFile, err := ioutil.ReadFile(filename)
 
-	for _, runnable := range runnables {
-		runnablesChannel := make(chan string)
-		channels[runnable.Name] = runnablesChannel
+// 		if err != nil {
+// 			panic(err)
+// 		}
 
-		wg.Add(1)
-		go runnableRoutine(runnable, feedbackChannel)
-	}
+// 		actions := Actions{}
+// 		err = yaml.Unmarshal(yamlFile, &actions)
 
-	go func() {
-		for {
-			select {
-			case msg := <-feedbackChannel:
-				fmt.Println(msg) // Uncomment to see the actions that have been run
-				wg.Done()
-				processCompletedRunnable(msg)
-			}
-		}
-	}()
+// 		if err != nil {
+// 			panic("Unmarshal failed")
+// 		}
 
-	wg.Wait()
-}
+// 		for _, currentAction := range actions.Actions {
+// 			runnables := currentAction.Runnables
+// 			runnablesStatuses[currentAction.Name] = make(map[string]Runnable)
 
-func processCompletedRunnable(runnableName string) {
-	for _, dependentToNotify := range dependentsMap[runnableName] {
-		dependentChannel := channels[dependentToNotify]
-		dependentChannel <- runnableName
-	}
-}
+// 			for _, runnable := range runnables {
+// 				runnablesStatuses[currentAction.Name][runnable.Name] = runnable
+// 			}
 
-func runnableRoutine(runnable Runnable, feedbackChannel chan string) {
-	runnableChannel := channels[runnable.Name]
+// 			for _, runnable := range runnables {
+// 				for _, dependent := range runnable.DependsOn {
+// 					dependentsMap[dependent] = append(dependentsMap[dependent], runnable.Name)
+// 				}
+// 			}
+// 		}
+// 	}
 
-	if len(runnable.DependsOn) == 0 {
-		runCommand(runnable, feedbackChannel)
-	} else {
-		for {
-			select {
-			case dependentCompleted := <-runnableChannel:
-				selectedIndex := -1
+// 	return runnablesStatuses
+// }
 
-				for index, dependent := range runnable.DependsOn {
-					if dependentCompleted == dependent {
-						selectedIndex = index
-					}
-				}
+// func runAction(actionName string) {
+// 	runnables := allRunnables[actionName]
+// 	feedbackChannel := make(chan string)
 
-				if selectedIndex > -1 {
-					// Remove element from array
-					runnable.DependsOn[selectedIndex] = runnable.DependsOn[len(runnable.DependsOn)-1]
-					runnable.DependsOn = runnable.DependsOn[:len(runnable.DependsOn)-1]
+// 	defer close(feedbackChannel)
 
-					if len(runnable.DependsOn) == 0 {
-						runCommand(runnable, feedbackChannel)
-					}
-				}
-			}
-		}
-	}
-}
+// 	var wg sync.WaitGroup
+
+// 	for _, runnable := range runnables {
+// 		runnablesChannel := make(chan string)
+// 		channels[runnable.Name] = runnablesChannel
+
+// 		wg.Add(1)
+// 		go runnableRoutine(runnable, feedbackChannel)
+// 	}
+
+// 	go func() {
+// 		for {
+// 			select {
+// 			case msg := <-feedbackChannel:
+// 				// fmt.Println(msg) // Uncomment to see the actions that have been run
+// 				wg.Done()
+// 				processCompletedRunnable(msg)
+// 			}
+// 		}
+// 	}()
+
+// 	wg.Wait()
+// }
+
+// func processCompletedRunnable(runnableName string) {
+// 	for _, dependentToNotify := range dependentsMap[runnableName] {
+// 		dependentChannel := channels[dependentToNotify]
+// 		dependentChannel <- runnableName
+// 	}
+// }
+
+// func runnableRoutine(runnable Runnable, feedbackChannel chan string) {
+// 	runnableChannel := channels[runnable.Name]
+
+// 	if len(runnable.DependsOn) == 0 {
+// 		runCommand(runnable, feedbackChannel)
+// 	} else {
+// 		for {
+// 			select {
+// 			case dependentCompleted := <-runnableChannel:
+// 				selectedIndex := -1
+
+// 				for index, dependent := range runnable.DependsOn {
+// 					if dependentCompleted == dependent {
+// 						selectedIndex = index
+// 					}
+// 				}
+
+// 				if selectedIndex > -1 {
+// 					// Remove element from array
+// 					runnable.DependsOn[selectedIndex] = runnable.DependsOn[len(runnable.DependsOn)-1]
+// 					runnable.DependsOn = runnable.DependsOn[:len(runnable.DependsOn)-1]
+
+// 					if len(runnable.DependsOn) == 0 {
+// 						runCommand(runnable, feedbackChannel)
+// 					}
+// 				}
+// 			}
+// 		}
+// 	}
+// }
 
 func runCommand(runnable Runnable, feedbackChannel chan string) {
-	feedbackChannel <- runnable.Name
-
 	// TODO: Run the command and wait for completion. Send feedback once completed
+
+	command := exec.Command("bash", "-c", "nvm use default")
+	out, err := command.CombinedOutput()
+
+	fmt.Println(err)
+	fmt.Println(string(out))
+
+	if err != nil {
+		// fmt.Println(err.Error())
+		// feedbackChannel <- runnable.Name //Blocking so never ends if you don't listen
+		return
+	}
+
+	// fmt.Println(string(out))
+	// feedbackChannel <- runnable.Name
 }
